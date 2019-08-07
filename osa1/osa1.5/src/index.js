8737a7124c7138e7157cc910caa786856bfd6a5b
Timo Kivinen <timo.j.kivinen@helsinki.fi>
2019-08-07 21:18:39 +0300

    Osa 1.5 Kurssitiedot step 5

--- osa1/osa1.5/src/index.js
+++ osa1/osa1.5/src/index.js
@@ -1,10 +1,9 @@
 // -----------------------------------------------------------
-// --- Tehtävä          1.5: kurssitiedot, step5
+// --- Tehtävä          1.5: kurssitiedot, step5 
 // --- Pvm:             6.4.2019 Timo Kivinen
 // ---                  4.6.2019 (siistitty)  
 // --- Aikaa käytetty:  3 h
 // -----------------------------------------------------------
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 
