9681d1c12bd7632f6fe70cd94a17ffb19c585112
timokivinen <48415563+timokivinen@users.noreply.github.com>
2019-11-10 18:33:37 +0200

    3.11 puhelinluettelo full stack

---
+++ osa3/osa3.11f/build/precache-manifest.03e4a41ee02519f07c5950c7df09f1b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95acdb5dd2259c0933658eeb17494a61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1816341d2b8b2b15704a",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "1816341d2b8b2b15704a",
+    "url": "/static/js/main.780e751d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
