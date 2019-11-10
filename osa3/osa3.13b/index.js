4842da8654760f8aab5844b6028c2b42c1411b6e
timokivinen <48415563+timokivinen@users.noreply.github.com>
2019-11-10 20:21:19 +0200

    3.13b: Backend, puhelinluettelo ja tietokanta, step1

---
+++ osa3/osa3.13b/index.js
@@ -0,0 +1,215 @@
+// --------------------------------------------------------------------------
+// --- Tehtävä:         3.13b: puhelinluettelo ja tietokanta, step1
+// --- Pvm:             20.8.2019 Timo Kivinen
+// --- Front End:       3.11f                 
+// --- Aikaa käytetty:  6 h
+// --------------------------------------------------------------------------
+// --- Muuta backendin kaikkien puhelintietojen näyttämistä siten, 
+// --- että se hakee näytettävät puhelintiedot tietokannasta.
+// --------------------------------------------------------------------------
+// --- Plug ins:            JSONview
+// --- Projektin luonti:    npm init
+// --- Express install:     npm install express --save
+// --- Nodemon install:     npm install --save-dev nodemon
+// --- Morgan install:      npm install morgan
+// --- Mongoose install:    npm install mongoose --save 
+// --- Cors install:        npm install cors --save
+// --- Käynnistys:          npm start/ node index.js password
+// --- Käynnistys nodemon:  npm run watch
+// --------------------------------------------------------------------------
+// --- URL	      verbi     toiminnallisuus
+// --- notes/10   GET	      hakee yksittäisen resurssin
+// --- notes	    GET	      hakee kokoelman kaikki resurssit
+// --- notes	    POST	    luo uuden resurssin pyynnön mukana olevasta datasta
+// --- notes/10	  DELETE    poistaa yksilöidyn resurssin
+// --- notes/10	  PUT	      korvaa yksilöidyn resurssin pyynnön mukana olevalla datalla
+// --- notes/10	  PATCH	    korvaa yksilöidyn resurssin osan pyynnön mukana olevalla datalla
+// --------------------------------------------------------------------------
+const Person = require('./models/person')
+
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+const bodyParser = require('body-parser')
+// -- Ilman body-parseria body arvo on undefined
+// -- Parser muuttaa pyynnön JSON-muotoisen datan ja muuttaa
+// --- sen Javascript-olioksi (==> request.body)
+app.use(bodyParser.json())
+
+// --- minimal output: :method :url :status :res[content-length] - :response-time ms
+// app.use(morgan('tiny'))
+app.use(morgan('combined'))
+
+const cors = require('cors')
+app.use(cors())
+
+// --------------------------------------------------------------------
+// --- Mongo Db params
+// --------------------------------------------------------------------
+const person_password = process.argv[2]
+const person_name = process.argv[3]
+const person_number = process.argv[4]
+console.log('argv.len', process.argv.length)
+
+  // ----------------------------------------------------------
+  // --- Morgan middleware                      (Teht. 3.8)
+  // --- :method :url :status :res[content-length] - :response-time ms
+  // ----------------------------------------------------------
+  morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+  var loggerFormat = 'logger: :method :url :status :res[content-length] - :response-time ms :body '
+  app.use(morgan(loggerFormat, {
+    skip: function (req, res) {
+      return res.statusCode >= 400
+    },
+    stream: process.stdout
+  }))
+
+// -----------------------------------------------------------
+let persons = [
+  {
+  id: 1,
+  name: "Arto Hellas",
+  number: "040-12345678"
+  },
+  {
+  id: 2,
+  name: "Jaakko Javanainen",
+  number: "050-5656567"
+  },
+  {
+  id: 3,
+  name: "Jorma Käteinen",
+  number: "045-66778899"
+  },
+  {
+  id: 4,
+  name: "Kauko Näköinen",
+  number: "045-1122112211"
+  },
+  {
+  id: 5,
+  name: "Jesse James",
+  number: "040-66677766677"
+  }
+]
+
+// ----------------------------------------------------------
+// --- Haetaan puhelintiedot tietokannasta      (Teht 3.13)
+// ----------------------------------------------------------
+app.get('/api/persons', (req, res) => {
+  console.log(`#---- Phonebook:`)
+  Person
+    .find({}).then(people => {
+    res.json(people)
+    Person.mongoose.connection.close()
+  })
+})
+// ----------------------------------------------------------
+// --- Palautetaan henkilöiden lukumäärä ja kellonaika (Teht 3.2)
+// ----------------------------------------------------------
+app.get('/api/info', (req, res) => {
+
+  let currTime = new Date()
+  const personCount = persons.length
+  
+  let responseText = `Phonebook has info for ${personCount} people `
+  responseText = responseText.concat(`<br/>${currTime} `)
+  
+  console.log(responseText)
+  res.send(responseText)
+
+})
+// ----------------------------------------------------------
+// --- Haetaan id:tä vastaavan henkilön tiedot (Teht 3.3)
+// ----------------------------------------------------------
+app.get('/api/persons/:id', (req, res) => {
+  // --- Muutetaan samaksi tyypiksi, jotta voidaan vertailla
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+      res.json(person)
+  } 
+  else {
+      res.status(404).end()      // Ei löytynyt
+  }
+})
+
+// ----------------------------------------------------------
+// --- Poisto / DELETE (Teht 3.4)
+// ----------------------------------------------------------
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
+  // ----------------------------------------------------------
+  // --- Käytetään spread-syntaksia: taulukko yksittäisiksi luvuiksi
+  // --- Jos rivejä olemassa, niin maksimi id +100
+  // ----------------------------------------------------------
+  const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+    return maxId + 100
+  }
+
+// ----------------------------------------------------------
+// --- Lisäys / POST (Teht 3.5/3.6)
+// --- Body-parser käytössä
+// ----------------------------------------------------------
+app.post('/api/persons', (req, res) => {
+  
+  //console.log("**", req.body, "**")
+  //console.log(req.headers)
+  let errMessage  = ""
+
+  if (!req.body.name) {       // --- Nimi puuttuu
+      errMessage = "{ error: 'Name is mandatory.' }"
+  }
+  if (!req.body.number) {     // --- Puhelinnumero puuttuu
+      errMessage = "{ error: 'Number is mandatory.' }"
+  }
+  // --- Onko tuplanimi?
+  const name = persons.find(person => person.name.toUpperCase() === req.body.name.toUpperCase())
+  if (name) {
+      errMessage = "{ error: 'Name must be unique.' }"
+  } 
+
+  if (errMessage) {
+      console.log(`${errMessage}. Nimi: ${req.body.name} Numero: ${req.body.number} \n`)
+      res.write(`${errMessage}`)
+      res.end('')
+      return 0
+  }
+
+  // --- Lisätään rivi luetteloon
+  const person = {
+    name: req.body.name,
+    number: req.body.number,
+    // -- content: req.body,
+    important: req.body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }        
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+// ----------------------------------------------------------
+// --- Unknown enpoint resides after routes
+// --- 
+// ----------------------------------------------------------
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  app.use(unknownEndpoint)
+
+// ----------------------------------------------------------
+// --- Kuunnellaan Porttia
+// ----------------------------------------------------------
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`***Server running on port ${PORT}`)
+})
---
+++ osa3/osa3.13b/models/person.js
@@ -0,0 +1,44 @@
+// --------------------------------------------------------------------------
+// --- Moduli:          persons.js
+// --- Tehtävä:         Tietokantamäärittelyt
+// --- Pvm:             20.8.2019 Timo Kivinen
+// ---                  
+// --------------------------------------------------------------------------
+
+// -----------------------------------------------------------
+// --- Mongoose
+// -----------------------------------------------------------
+const mongoose = require('mongoose')
+// --------------------------------------------------------------------
+// --- Syntax: node mongo.js password "firstname lastname" phonenumber
+// --------------------------------------------------------------------
+const person_password = process.argv[2]
+// const url = process.env.MONGODB_URI
+const url =
+`mongodb+srv://fullstack:${person_password}@cluster0-d6pl0.mongodb.net/person?retryWrites=true&w=majority`
+
+console.log('Connecting to ', url)
+
+mongoose.connect(url, { useNewUrlParser: true })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+const Person = mongoose.model('Person', personSchema)
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
