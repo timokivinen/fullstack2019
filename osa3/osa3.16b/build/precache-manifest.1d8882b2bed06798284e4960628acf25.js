d457e809c36c891ebe989a85c660d94add1a9cde
timokivinen <48415563+timokivinen@users.noreply.github.com>
2019-11-17 13:41:48 +0200

    3.16: puhelinluettelo ja tietokanta, step3 BackEnd

---
+++ osa3/osa3.16b/build/precache-manifest.1d8882b2bed06798284e4960628acf25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "636236f17c7a10d49865ebc2d6d648b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "920850363aabca67c460",
+    "url": "/static/css/main.7c0f2ffb.chunk.css"
+  },
+  {
+    "revision": "e409f7b3ead562cb18b4",
+    "url": "/static/js/2.b0457393.chunk.js"
+  },
+  {
+    "revision": "920850363aabca67c460",
+    "url": "/static/js/main.f9e2fa7f.chunk.js"
+  },
+  {
+    "revision": "ae39999ae25750bd3076",
+    "url": "/static/js/runtime~main.db810ea8.js"
+  }
+]);
