76f5b6bb4cd5e6fc4cb934db8365318d4f7e648c
timokivinen <48415563+timokivinen@users.noreply.github.com>
2019-11-17 13:59:17 +0200

    3.22b tietokantaa käyttävä versio internettiin BackEnd

---
+++ osa3/osa3.22b/build/precache-manifest.acdb8f17768abe87ae46a73c50adc068.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb324015359d0cbab0656bfee550343",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9cd2add335b11d5e66b",
+    "url": "/static/css/main.d0d23d09.chunk.css"
+  },
+  {
+    "revision": "287dadc56edd0d141402",
+    "url": "/static/js/2.34b27415.chunk.js"
+  },
+  {
+    "revision": "b9cd2add335b11d5e66b",
+    "url": "/static/js/main.76147337.chunk.js"
+  },
+  {
+    "revision": "7f05dee86618504c731d",
+    "url": "/static/js/runtime~main.aa08122f.js"
+  }
+]);
