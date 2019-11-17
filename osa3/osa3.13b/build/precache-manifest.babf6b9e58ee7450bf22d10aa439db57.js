33a70c2974e0d332e0bfc1366ac77e333bb0ab00
timokivinen <48415563+timokivinen@users.noreply.github.com>
2019-11-17 13:20:36 +0200

    Add files via upload

---
+++ osa3/osa3.13b/build/precache-manifest.babf6b9e58ee7450bf22d10aa439db57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e45df8521d949f0fa5cebd1990d57eab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59e3b1140b0fd6c578d6",
+    "url": "/static/css/main.7c0f2ffb.chunk.css"
+  },
+  {
+    "revision": "f010d8a1a2e85aaecf77",
+    "url": "/static/js/2.b428dcb5.chunk.js"
+  },
+  {
+    "revision": "59e3b1140b0fd6c578d6",
+    "url": "/static/js/main.2aa14884.chunk.js"
+  },
+  {
+    "revision": "de030dd1d8577c8468f4",
+    "url": "/static/js/runtime~main.e7e3b390.js"
+  }
+]);
